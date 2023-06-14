d22209c78f28099f6959c711855bd5aec56c8741
Dev <devextraemail@gmail.com>
2023-06-14 01:02:23 -0400

    Caught errors, changed bad routing, removed unused imports

--- routes_api.py
+++ routes_api.py
@@ -1,17 +1,14 @@
 # Imports of flask and SQLALCHEMY all done within virtualenv 
-from flask import Blueprint, render_template, request, url_for, redirect, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from models import User, Content, Log, db
-from flask_bcrypt import Bcrypt
-from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, jsonify
+from models import Content
 
 #Create Flask blueprint object
 api = Blueprint('api', __name__)
 
 
 #Simple default route for the index that pulls from the templates folder by default index.html
-@api.route('/api/allcontent')
-def apiTest():
+@api.route('/api/content/all', methods=['GET'])
+def get_all_content():
     allContent = Content.query.all()
     data ={}
     for content in allContent:
