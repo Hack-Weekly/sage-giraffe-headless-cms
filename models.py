ef0485fd35a6d1b0fbc9758777b36e71616d0443
Dev <devextraemail@gmail.com>
2023-06-14 20:43:52 -0400

    Added log to models from earlier merge revert

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -14,6 +15,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(50), nullable=False)
     lastLogin = db.Column(db.DateTime, default=datetime.utcnow) # -5 hours for EST
+
+    # Define the relationship to Content model
+    logs = relationship('Log', backref='user_logs')
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -43,3 +47,17 @@ class Content(db.Model, UserMixin):
             'userId': self.userId
         }
     
+
+class Log(db.Model):
+    __tablename__ = 'activity_logs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('user_logs', lazy=True))
+    action = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # -5 hours for EST
+
+
+    def __repr__(self):
+        return '<Log %r>' % self.id
+
